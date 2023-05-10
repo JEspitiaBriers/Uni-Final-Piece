b1c076e19803369e80e95d3887b4fb1acc5c5a75
JEspitiaBriers <19021656@stu.mmu.ac.uk>
2023-05-10 10:38:57 +0100

    revert, login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 
-import { Form, Field, ErrorMessage } from 'vee-validate'
 import { validateEmail, checkPasswordLength } from '../assets/utils.js'
 
 import { firebaseAuthentication, signInWithEmailAndPassword } from '@/firebase/database'
-
 import { useRouter } from 'vue-router'
 
+const email = ref('')
+const password = ref('')
 const loginMessage = ref(null)
 
 const router = useRouter()
@@ -22,21 +22,21 @@ function passwordToggle() {
 }
 
 function login() {
-  signInWithEmailAndPassword(firebaseAuthentication, document.getElementById('loginEmail').value, document.getElementById('loginPassword').value).then(
-    (userCredential) => {
+  signInWithEmailAndPassword(firebaseAuthentication, email.value, password.value)
+    .then((userCredential) => {
+      console.log(userCredential.user)
       router.push("/")
     },
-    (error) => {
-      if (error.message.substring(error.message.indexOf("/") + 1, error.message.length - 2) == "internal-error") {
-        loginMessage.value = "missing-field"
+      (error) => {
+        if (error.message.substring(error.message.indexOf("/") + 1, error.message.length - 2) == "internal-error") {
+          loginMessage.value = "missing-field"
+        }
+        else {
+          loginMessage.value = error.message.substring(error.message.indexOf("/") + 1, error.message.length - 2)
+        }
       }
-      else {
-        loginMessage.value = error.message.substring(error.message.indexOf("/") + 1, error.message.length - 2)
-      }
-    }
-  )
+    )
 }
-
 function resetPassword() {
   router.push('/resetpassword')
 }
@@ -44,30 +44,38 @@ function resetPassword() {
 
 <template>
   <div class="onPage" id="loginPage">
-    <h2 style="text-align: center">Login</h2>
+    <template v-if="!user">
+      <h2 style="text-align: center">Login</h2>
 
-    <label>Email</label>
-    <input class="form-control" id="loginEmail" type="email" placeholder="eg. some.fancy@email.com"
-      :rules="validateEmail" />
+      <label>Email</label>
+      <input class="form-control" v-model="email" type="email" placeholder="eg. some.fancy@email.com"
+        :rules="validateEmail" />
 
-    <label>Password</label>
-    <input class="form-control" id="loginPassword" type="password" placeholder="Password" :rules="checkPasswordLength" />
+      <label>Password</label>
+      <input class="form-control" id="loginPassword" v-model="password" type="password" placeholder="Password"
+        :rules="checkPasswordLength" />
 
-    <div class="input-group" style="margin-top: 3px; margin-left: 25%;">
-      <div class="input-group-text">
-        <input type="checkbox" @click="passwordToggle()">
+      <div class="input-group" style="margin-top: 3px; margin-left: 25%;">
+        <div class="input-group-text">
+          <input type="checkbox" @click="passwordToggle()">
+        </div>
+        <div class="input-group-append">
+          <span class="input-group-text">Show Passwords</span>
+        </div>
       </div>
-      <div class="input-group-append">
-        <span class="input-group-text">Show Passwords</span>
-      </div>
-    </div>
 
-    <div v-if="loginMessage" style="text-align: center">{{ loginMessage }}</div>
+      <div v-if="loginMessage" style="text-align: center">{{ loginMessage }}</div>
 
-    <button class="w-100 btn btn-lg btn-success" style="margin-top: 5px" @click="login()">Login</button>
-    <button class="w-50 btn btn-sm btn-secondary" style="margin: 3px 0px 0px 25%;" @click="resetPassword()">Reset
-      Password</button>
-    <router-link to="/register" style="margin: 0px 0px 0px 34%;">Need to register?</router-link>
+      <button class="w-100 btn btn-lg btn-success" style="margin-top: 5px" @click="login()">Login</button>
+      <button class="w-50 btn btn-sm btn-secondary" style="margin: 3px 0px 0px 25%;" @click="resetPassword()">Reset
+        Password</button>
+      <router-link to="/register" style="margin: 0px 0px 0px 34%;">Need to register?</router-link>
+    </template>
+    <template v-else>
+      <h2 style="text-align: center">You're already logged in.</h2>
+      <p>Don't worry, you can click this button to head to the dashboard!</p>
+      <button class="w-50 btn btn-sm btn-secondary" style="margin: 3px 0px 0px 25%;" to="/">Home Page</button>
+    </template>
   </div>
 </template>
 
