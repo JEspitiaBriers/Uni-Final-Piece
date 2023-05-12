31c7b7b6b928c771e9ffcf1e02113eac120de07f
JEspitiaBriers <19021656@stu.mmu.ac.uk>
2023-05-12 10:38:52 +0100

    converted to setup no export

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -12,31 +12,25 @@
   </div>
 </template>
 
-<script>
+
+<script setup>
 import { StripeCheckout } from '@vue-stripe/vue-stripe'
-export default {
-  components: {
-    StripeCheckout
-  },
-  data() {
-    this.publishStripeKey = 'pk_test_51N3jriDC5rlXGg8WXEFMQPOKLX5QwVNge0MJL5aheSRDqDd10IY1qylyZGR9WJabU1H2nG4FQCo5eE4CBZxCLrnB009VzMkubq'
-    return {
-      loading: false,
-      purchaseItems: [{
-        price: 'price_1N5vgrDC5rlXGg8WVo7SitvU',
-        quantity: 1
-      }],
-      successURL: 'http://localhost:5173/paymentSuccess',
-      cancelURL: 'http://localhost:5173/paymentCancelled'
-    }
-  },
-  methods: {
-    toPayment() {
-      console.log("Submitting Test")
-      this.$nextTick(() => {
-        this.$refs.refCheckout.redirectToCheckout()
-      })
-    }
-  }
+import { ref } from 'vue'
+
+const publishStripeKey = 'pk_test_51N3jriDC5rlXGg8WXEFMQPOKLX5QwVNge0MJL5aheSRDqDd10IY1qylyZGR9WJabU1H2nG4FQCo5eE4CBZxCLrnB009VzMkubq'
+
+const purchaseItems= [{
+  price: 'price_1N5vgrDC5rlXGg8WVo7SitvU',
+  quantity: 1
+}]
+
+const successURL = 'http://localhost:5173/paymentSuccess'
+const cancelURL = 'http://localhost:5173/paymentCancelled'
+
+const refCheckout = ref(null)
+
+async function toPayment() {
+  console.log("Submitting Test")
+  await refCheckout.value.redirectToCheckout()
 }
 </script>
